6d5d01aff5293f31d07eadea11e2545bff7d8638
Nadya <nadya0707@yandex.ru>
2023-02-17 21:07:06 +0300

    commit 1

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, 
+// которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+
+void InputArray (int[,,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      for (int k = 0; k < array.GetLength(1); k++)
+      {
+        Console.Write($"{array[i,j,k]} ({i}, {j}, {k})\t");
+      }
+      Console.WriteLine();
+    }
+  }
+}
+void Release(int[,,] array)
+{
+  int[] temp = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
+  int  number;
+  for (int i = 0; i < temp.GetLength(0); i++)
+  {
+    temp[i] = new Random().Next(10, 100);
+    number = temp[i];
+    if (i >= 1)
+    {
+      for (int j = 0; j < i; j++)
+      {
+        while (temp[i] == temp[j])
+        {
+          temp[i] = new Random().Next(10, 100);
+          j = 0;
+          number = temp[i];
+        }
+          number = temp[i];
+      }
+    }
+  }
+  int count = 0; 
+  for (int x = 0; x < array.GetLength(0); x++)
+  {
+    for (int y = 0; y < array.GetLength(1); y++)
+    {
+      for (int z = 0; z < array.GetLength(2); z++)
+      {
+        array[x, y, z] = temp[count];
+        count++;
+      }
+    }
+  }
+}
+Console.WriteLine($"Введите размер массива: ");
+int [ ] size = Console.ReadLine().Split(" ").Select(x=>int.Parse(x)).ToArray();
+int[,,] array = new int[size[0], size [1], size [2]];
+Release(array);
+InputArray(array);
